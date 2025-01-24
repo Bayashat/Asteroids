1fb34e26d3b4679180a8b53855b0d84b1d847389
Bayashat <baiashat2002@gmail.com>
2025-01-25 00:00:22 +0500

    feat:Add asteroid splitting functionality on collision with shots

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
 
     asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
@@ -28,19 +27,30 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
         for obj in updatable:
             obj.update(dt)
-        for obj in drawable:
-            obj.draw(screen)
+
         for asteroid in asteroids:
             if player.collides_with(asteroid):
                 return
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the frame rate to 60 FPS
